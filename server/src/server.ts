9a490ba9cdec1fbc4996d4d08478b324bc176bd7
Leandro Acquati <leandroacquati@gmail.com>
2020-08-15 20:27:25 -0300

    update readme

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,21 @@ import express, { response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 // Métodos API Rest
 // GET: Buscar ou listar uma informação
 // POST: Criar alguma nova informação
 // PUT: Atualizar uma informação existente
 // DELETE: Deletar uma informação existente
+
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Paginação, filtros, ordenação
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World!' });
+});
 
 app.get('/users', (request, response) => {
   // Aparece no console do server:
@@ -14,6 +24,8 @@ app.get('/users', (request, response) => {
 
   // Aparece no navegador na forma de um html cru:
   // return response.send('Hello World!');
+
+  console.log(request.query);
 
   const users = [
     { name: 'Diego', age: 25 },
@@ -24,6 +36,19 @@ app.get('/users', (request, response) => {
 });
 
 app.post('/users', (request, response) => {
+  console.log(request.body);
+
+  const users = [
+    { name: 'Diego', age: 25 },
+    { name: 'Vini', age: 21 }
+  ];
+
+  return response.json(users);
+});
+
+app.delete('/users/:id', (request, response) => {
+  console.log(request.params);
+
   const users = [
     { name: 'Diego', age: 25 },
     { name: 'Vini', age: 21 }
