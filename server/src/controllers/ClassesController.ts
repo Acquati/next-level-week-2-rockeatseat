9c49bc4c7ebb2f39fc8b6a306d7be8f99ab876da
Leandro Acquati <leandroacquati@gmail.com>
2020-08-16 03:10:46 -0300

    add classes create method

---
+++ server/src/controllers/ClassesController.ts
@@ -0,0 +1,78 @@
+// Métodos API Rest
+// GET: Buscar ou listar uma informação
+// POST: Criar alguma nova informação
+// PUT: Atualizar uma informação existente
+// DELETE: Deletar uma informação existente
+
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Paginação, filtros, ordenação
+
+import { Request, Response } from 'express';
+
+import db from '../database/connection';
+import convertHourToMinutes from '../util/convertHourToMinutes';
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+export default class ClassesController {
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost,
+      schedule
+    } = request.body;
+
+    const trx = await db.transaction();
+
+    try {
+      const insertedUsersIds = await trx('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio
+      });
+
+      const user_id = insertedUsersIds[0];
+
+      const insertedClassesIds = await trx('classes').insert({
+        subject,
+        cost,
+        user_id
+      });
+
+      const class_id = insertedClassesIds[0];
+
+      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: convertHourToMinutes(scheduleItem.from),
+          to: convertHourToMinutes(scheduleItem.to)
+        };
+      });
+
+      await trx('class_schedule').insert(classSchedule);
+
+      await trx.commit();
+
+      return response.status(201).send();
+    } catch (err) {
+      console.log(err);
+
+      await trx.rollback();
+
+      return response.status(400).json({
+        error: 'Unexpected error while creating new class'
+      });
+    }
+  }
+}
