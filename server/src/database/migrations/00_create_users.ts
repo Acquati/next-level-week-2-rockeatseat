2930f7a1b7563248c1f31f61f27729aafa1d1b69
Leandro Acquati <leandroacquati@gmail.com>
2020-08-15 21:45:55 -0300

    add migrations

--- server/src/database/migrations/00_create_users.ts
+++ server/src/database/migrations/00_create_users.ts
@@ -2,7 +2,8 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
   return knex.schema.createTable('users', table => {
-    table.increments('id').primary;
+    table.increments('id').primary();
+
     table.string('name').notNullable();
     table.string('avatar').notNullable();
     table.string('whatsapp').notNullable();
